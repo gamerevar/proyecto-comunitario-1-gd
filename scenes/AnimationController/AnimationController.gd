5d96918d301cbfd459131f6ea3bc9669f9775c33
Juan José Montes de Oca Arbós <juanjose.montesdeocaarbos@gmail.com>
2021-01-04 06:28:11 -0300

    Se utiliza la animación indicada como argumento de la función

--- scenes/AnimationController/AnimationController.gd
+++ scenes/AnimationController/AnimationController.gd
@@ -4,9 +4,7 @@ onready var _animation_tree := $AnimationTree
 onready var _animation_state : AnimationNodeStateMachinePlayback = _animation_tree.get("parameters/playback")
 
 func play_animation(name: String, direction: Vector2) -> void:
-	if direction == Vector2.ZERO:
-		_animation_state.travel("Idle")
-	else:
+	if direction != Vector2.ZERO:
 		_animation_tree.set("parameters/Idle/blend_position", direction)
-		_animation_tree.set("parameters/Walk/blend_position", direction)
-		_animation_state.travel("Walk")
+		_animation_tree.set("parameters/" + name + "/blend_position", direction)
+	_animation_state.travel(name)
