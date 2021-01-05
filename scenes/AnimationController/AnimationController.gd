36ccf7ef51abc0f5fb5cb9075b3641d14d33d782
Juan José Montes de Oca Arbós <juanjose.montesdeocaarbos@gmail.com>
2021-01-05 06:23:13 -0300

    Se cambian los nombres de las animaciones a snake_case

--- scenes/AnimationController/AnimationController.gd
+++ scenes/AnimationController/AnimationController.gd
@@ -5,6 +5,6 @@ onready var _animation_state : AnimationNodeStateMachinePlayback = _animation_tr
 
 func play_animation(name: String, direction: Vector2) -> void:
 	if direction != Vector2.ZERO:
-		_animation_tree.set("parameters/Idle/blend_position", direction)
+		_animation_tree.set("parameters/idle/blend_position", direction)
 		_animation_tree.set("parameters/" + name + "/blend_position", direction)
 	_animation_state.travel(name)
