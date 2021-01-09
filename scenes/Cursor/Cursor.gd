3661f6753013b3756562fdfa0b068f15acd75bd6
Leando Cuello <leandroezequiel1999@gmail.com>
2021-01-08 21:06:26 -0300

    now selectes now walkable tile & add Modes

--- scenes/Cursor/Cursor.gd
+++ scenes/Cursor/Cursor.gd
@@ -1,59 +1,86 @@
 extends Node2D
 
+enum Mode { 
+	IDLE,
+	UNIT_CAN_REACH,
+	UNIT_CANT_REACH
+}
+
 export var navigation_2d_node_path : NodePath
 
-var selected_tile : Vector2
+var current_Mode = Mode.IDLE
 var current_tile : Vector2
-var selected_unit : Area2D
 var current_unit : Area2D
+var selected_unit : Area2D
 
 onready var mouse_container := $MouseContainer #containes everything that moves with the Cursor
 onready var Tile_container := $TileContainer #containes everything that moves depending the grid
 onready var tile_map := get_parent()
 onready var navigation_2d := get_node(navigation_2d_node_path)
 
-func _unhandled_input(event):
-	var mouse_position := get_global_mouse_position()
+func _ready()-> void:
+	change_mode(Mode.IDLE)
 	
-	mouse_container.global_position = mouse_position
+
+func _unhandled_input(event)-> void:
+	var mouse_position := get_global_mouse_position()
 	current_tile = tile_map.world_to_map(mouse_position)
 	
-	if is_valid_tile(current_tile): 
-		update_selected_tile(current_tile)
+	mouse_container.global_position = mouse_position
+	Tile_container.global_position = tile_map.map_to_world(current_tile)
+	
+	check_tile_state()
 	
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_RIGHT and selected_unit != null:
-			create_and_send_path(tile_map.map_to_world(selected_tile))
+		if event.button_index == BUTTON_RIGHT and current_Mode == Mode.UNIT_CAN_REACH:
+			create_and_send_path(tile_map.map_to_world(current_tile))
 		
-		#this is just for testing. and picking a unit by clicking
+		#DELETE when units are seleted from an other place
 		if event.button_index == BUTTON_LEFT and current_unit != null:
-			selected_unit = current_unit
-			selected_unit.select()
+			set_unit(current_unit)
 		
 	
 
-func update_selected_tile(tile)-> void:
-	selected_tile = tile
-	Tile_container.global_position = tile_map.map_to_world(selected_tile)
+func change_mode(mode := Mode.NONE)-> void:
+	current_Mode = mode
+	match current_Mode:
+		Mode.IDLE:
+			$TileContainer/WalkTileSprite.visible = false
+			$TileContainer/NotWalkTileSprite.visible = false
+		Mode.UNIT_CAN_REACH:
+			$TileContainer/WalkTileSprite.visible = true
+			$TileContainer/NotWalkTileSprite.visible = false
+		Mode.UNIT_CANT_REACH:
+			$TileContainer/WalkTileSprite.visible = false
+			$TileContainer/NotWalkTileSprite.visible = true
+	
 
-func is_valid_tile(tile)-> bool:
-	#this might need change depending the how we finaly set the tilemaps
-	if tile_map.get_cellv(tile) == 2:
-		return true
-	return false
+func check_tile_state()-> void:
+	if selected_unit == null:
+		return
+	
+	if tile_map.get_cellv(current_tile) == 2:
+		change_mode(Mode.UNIT_CAN_REACH)
+	else:
+		change_mode(Mode.UNIT_CANT_REACH)
 
 func create_and_send_path(endpoint)-> void:
 	var start_point = selected_unit.global_position
 	endpoint = endpoint + Vector2(0, 8) #for setting it in the middle of the tile
 	
 	var path = navigation_2d.get_simple_path(start_point, endpoint)
-	path.remove(0)# Because Unit dont need current position.
 	
 	selected_unit._update_way(path)
 
-#for storing wich unit the mouse is hovering over
+func set_unit(unit: Area2D = null):
+	selected_unit = unit
+	selected_unit.select() #DELETE just for testing.
+
+#for storing wich unit the mouse is hovering.
+#DELETE when units are seleted from an other place
 func _on_AllyArea_area_entered(area)-> void:
 	current_unit = area
 
-func _on_AllyArea_area_exited(area)-> void:
+
+func _on_AllyArea_area_exited(_area)-> void:
 	current_unit = null
